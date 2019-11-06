36816d2022e13a0283245f11c4735fe73071812c
Нина <young_imposer@mail.ru>
2019-11-06 18:31:23 +0300

    Записано все задание БОРЬБА ЗА КРЕСЛО

--- code.js
+++ code.js
@@ -201,19 +201,81 @@ console.log("Split array is ", vals);
 
 //Борьба за кресло
 
-function validate(phoneNumber){
-  if (phoneNumber.length !== 8) {
+// function validate_1(phoneNumber){
+//   if (phoneNumber.length !== 8) {
+//     return false;
+//   }
+//   for (var i = 0; i < phoneNumber.length; i++){
+//     if (i === 3) {
+//       if (phoneNumber.charAt(i) !== '-') {
+//         return false;
+//       }
+//     } else if (isNaN(phoneNumber.charAt(i))) {
+//       return false;
+//     }
+//   }
+//   return true;
+
+// }
+
+// function validate_2(phoneNumber){
+//   if (phoneNumber.length !== 8) {
+//     return false;
+//   }
+//   var first = phoneNumber.substring(0,3);
+//   var second = phoneNumber.substring(4);
+//   if (phoneNumber.charAt(3) !== "-" || isNaN(first) || isNaN(second)) {
+//     return false;
+//   }
+//   return true;
+// }
+
+function validate_1(phoneNumber) {
+  if (phoneNumber.length > 8 || phoneNumber.length < 7) {
     return false;
   }
-  for (var i = 0; i < phoneNumber.length; i++){
-    if (i === 3) {
-      if (phoneNumber.charAt(i) !== '-') {
+    for (var i = 0; i < phoneNumber.length; i++) {
+      if (i === 3){
+        if (phoneNumber.length === 8 &&
+          phoneNumber.charAt(i) !== '-'){
+            return false;
+          } else if (phoneNumber.length === 7 &&
+            isNaN(phoneNumber.charAt(i))) {
+              return false;
+            }
+      } else if (isNaN(phoneNumber.charAt(i))) {
         return false;
       }
-    } else if (isNaN(phoneNumber.charAt(i))) {
+    }
+    return true;
+}
+
+function validate_2(phoneNumber) {
+  if (phoneNumber.length > 8 ||
+    phoneNumber.length < 7) {
       return false;
     }
+  var first = phoneNumber.substring(0,3);
+  var second = phoneNumber.substring(phoneNumber.length - 4);
+
+  if (isNaN(first) || isNaN(second)) {
+    return false;
+  }
+  if (phoneNumber.length === 8) {
+    return (phoneNumber.charAt(3) === '-');
   }
   return true;
 }
+
+function validate(phoneNumber) {
+  return phoneNumber.match(/^\d{3}-?\d{4}$/);
+}
+
+var phoneNumber = prompt("Введите номер вашего телефона");
+var tel = validate(phoneNumber);
+var tel_1 = validate_1(phoneNumber);
+var tel_2 = validate_2(phoneNumber);
+console.log(tel);    //Выводится null
+console.log(tel_1);
+console.log(tel_2);
 
