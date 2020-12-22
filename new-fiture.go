63d2430f49374ac26b1f6c2643bb6db82494ee5a
Нина <young_imposer@mail.ru>
2020-12-22 19:33:29 +0300

    conflict

--- new-fiture.go
+++ new-fiture.go
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 "Another new entry" 
 "Second entry in the file" 
 "content" 
-=======
-"Another new entry" 
-"Second entry in the file" 
-"content" 
->>>>>>> daa3a53... Added chenges in files
